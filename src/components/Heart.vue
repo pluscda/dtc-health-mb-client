cb3e025c444cc489ecdf4c11b68ef9faec103ab6
alex <pluscda@gmail.com>
2020-12-31 15:26:01 +0800

    g

--- src/components/Heart.vue
+++ src/components/Heart.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="dtc-head-cancer">
     <van-nav-bar title="心血管疾病線上諮詢" left-text="返回" left-arrow @click-left="onClickLeft" @click-right="onClickRight" />
+    <main class="px-2">
+      <p class="py-3">
+        心血管疾病（英語：cardiovascular
+        disease，簡稱CVD）指的是關於心臟或血管的疾病，又稱為循環系統疾病、迴圈系統疾病。常見的心血管疾病包括冠狀動脈症候群(心肌梗塞)、中風、高血壓性心臟病（英語：hypertensive
+        heart disease）、風濕性心臟病、動脈瘤、心肌病變、心房顫動、先天性心臟病、心內膜炎、以及周邊動脈阻塞性疾病等等。
+      </p>
+    </main>
   </section>
 </template>
 
@@ -30,19 +37,20 @@ export default {
 
 <style lang="scss" scoped>
 .dtc-head-cancer {
-  background: white;
+  background: black;
   width: 100vw;
-  color: #3570ec;
+  color: white;
   display: grid;
   grid-template-rows: max-content 1fr;
   position: relative;
   min-height: 100vh;
-  margin-bottom: 70px;
+}
+p {
+  line-height: 22px;
 }
 .grid-2-img {
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: repeat(auto-fit, 150px);
   gap: 0.2rem;
   > div {
     position: relative;
