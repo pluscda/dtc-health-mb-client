5793ce3c0c58a6debb70a687f7c38c1cd4ffe4b5
alex <pluscda@gmail.com>
2021-01-05 14:17:01 +0800

    g

--- src/views/DoctorDetails.vue
+++ src/views/DoctorDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="dtc-detail">
-    <van-nav-bar title="頭頸部" left-text="返回" left-arrow @click-left="onClickLeft" />
+    <van-nav-bar title="線上預約" left-text="返回" left-arrow @click-left="onClickLeft" />
     <main class="dtc-detail__content">
       <img src="https://unsplash.it/80/80" class="left" />
       <div class="right mt-2">
@@ -23,15 +23,15 @@
         <h4 class="pt-3" style="color:var(--bs-orange)">2000<span style="font-size:15px;">元</span></h4>
         <nav class="how2-subtitle mt-2">
           <div>
-            <van-icon name="medal" size="24px" color="var(--orange)" />
+            <van-icon name="medal" size="20px" color="var(--orange)" />
             <p>醫生真實</p>
           </div>
           <div>
-            <van-icon name="coupon" size="24px" color="var(--orange)" />
+            <van-icon name="coupon" size="20px" color="var(--orange)" />
             <p>未接診隨時退</p>
           </div>
           <div>
-            <van-icon name="service" size="24px" color="var(--orange)" />
+            <van-icon name="service" size="20px" color="var(--orange)" />
             <p>不滿意可申訴退款</p>
           </div>
         </nav>
@@ -39,12 +39,28 @@
           <p>1. 用戶透過實體寄件方式,寄出病史。</p>
           <p>2. 醫生問診不限交流次數。</p>
           <p>3. 醫生給出明確建議後問診結束。</p>
-          <p>4. 醫生接診後問診最長開啟72小時, 預期自動結束。</p>
+          <p>4. 醫生接診後問診最長開啟48小時, 預期自動結束。</p>
         </footer>
       </main>
     </nav>
-    <h4 class="mt-2">服務流程</h4>
-    <footer class="detail-footer"></footer>
+    <h4 class="mt-4" style="transform:translateX(10px)">服務流程</h4>
+    <footer class="detail-footer pb-5 pr-1">
+      <span data-msg="線上預約">
+        <img src="card.svg" />
+      </span>
+      <van-icon name="arrow" />
+      <span data-msg="病情描述">
+        <img src="note.svg" />
+      </span>
+      <van-icon name="arrow" />
+      <span data-msg="醫生回覆">
+        <img src="send.svg" />
+      </span>
+      <van-icon name="arrow" />
+      <span data-msg="問診結束">
+        <img src="medical-personnel-doctor.svg" />
+      </span>
+    </footer>
   </section>
 </template>
 
@@ -104,7 +120,11 @@ export default {
   grid-column: 1/-1;
   display: grid;
   grid-template-columns: repeat(3, max-content);
+  grid-gap: 3px;
   justify-content: space-around;
+  p {
+    font-size: 14px;
+  }
   > div {
     display: grid;
     grid-template-columns: max-content max-content;
@@ -121,6 +141,33 @@ export default {
 }
 .detail-footer {
   display: grid;
-  grid-template-columns: repeat(12, max-conetnet);
+  grid-template-columns: repeat(7, max-content);
+  justify-content: space-around;
+  padding-top: 20px;
+  > span {
+    border: 1px solid var(--bs-orange);
+    display: grid;
+    place-items: center;
+    width: 60px;
+    height: 60px;
+    border-radius: 100%;
+    position: relative;
+    &::after {
+      position: absolute;
+      content: attr(data-msg);
+      font-size: 14px;
+      bottom: 0px;
+      display: black;
+      left: 0%;
+      width: 100px;
+      transform: translateY(20px);
+      color: var(--dark);
+    }
+  }
+  i {
+    transform: translateY(22px);
+    color: var(--bs-orange);
+    font-size: 20px;
+  }
 }
 </style>
