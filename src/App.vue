3175df9ef29d470d493ae4a26f0944d3a26eeff0
alex <pluscda@gmail.com>
2021-01-08 09:39:20 +0800

    g

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
     <van-share-sheet v-model="showShareSheet" title="用地圖找名醫會館相關醫院" :options="gisOptions" @select="onSelectGis" :cancel-text="cancel" />
     <van-tabbar v-model="active" style="z-index:8;">
-      <nav class="gis-btn" @click="openGisOps">
+      <nav class="gis-btn" @click="openGisOps" data-msg="DTC MAP">
         <img src="pen.svg" />
       </nav>
       <van-tabbar-item icon="wap-home-o" @click="tabClick('/home')">{{ $t("醫療首頁") }}</van-tabbar-item>
